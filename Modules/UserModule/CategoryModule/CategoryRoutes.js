13844527770dce6619c2f9f7519318106c64c4d5
Wessal Ayman <wessalayman17@gmail.com>
2025-02-09 19:43:01 +0200

    updating  category

--- Modules/UserModule/CategoryModule/CategoryRoutes.js
+++ Modules/UserModule/CategoryModule/CategoryRoutes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { authenticated, authorizedAdmin } from "../../../src/utilities/authMiddleware.js";
-import { createCategory } from "./CategoryController.js";
+import { createCategory, updateCategory } from "./CategoryController.js";
 
 const category_routes = Router()
 
-category_routes.route("/categories")
-.post(authenticated, authorizedAdmin, createCategory
-);
+category_routes.route("/category").post(authenticated, authorizedAdmin, createCategory);
+category_routes.route("/:categoryId").put(authenticated, authorizedAdmin, updateCategory);
+
 
 export default category_routes
