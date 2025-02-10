6dc6f2a77c55347ba26ba48f9f33c3d5feddd39b
RahmaRubi <rahmaelroby47@gmail.com>
2025-02-11 00:56:26 +0200

    view and clear user cart

--- Modules/CartModule/CartRoutes.js
+++ Modules/CartModule/CartRoutes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { addToCart, deleteProductFromCart, updateProductQuantity } from "../CartModule/CartController.js";
+import { addToCart, clearCart, deleteProductFromCart, updateProductQuantity, viewCart } from "../CartModule/CartController.js";
 import { authenticated, authorizedAdmin } from "../../src/utilities/authMiddleware.js";
 export const cart_routes = Router()
 
 cart_routes.post("/cart/add", authenticated, addToCart)
 cart_routes.put("/cart/update", authenticated, updateProductQuantity)
-cart_routes.put("/cart/delete", authenticated, deleteProductFromCart)
+cart_routes.delete("/cart/delete", authenticated, deleteProductFromCart)
+cart_routes.delete("/cart/clearcart", authenticated, clearCart)
+cart_routes.get("/cart/view", authenticated, viewCart)
