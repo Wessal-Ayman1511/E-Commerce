a32cc097b2f88e6fc5c05316840ce59fca6dc920
Wessal Ayman <wessalayman17@gmail.com>
2025-02-10 23:06:27 +0200

    all product routes

--- Modules/ProductModule/ProductRoutes.js
+++ Modules/ProductModule/ProductRoutes.js
@@ -2,10 +2,26 @@ import { Router } from "express";
 import formidable from "express-formidable";
 import { authenticated, authorizedAdmin } from "../../src/utilities/authMiddleware.js";
 import checkId from "../../src/middlewares/checkId.js";
-import { addProduct } from "./ProductController.js";
+import { addProduct, addProductReview, allProductsWithSearchKeyword, deleteProduct, fetchAllProducts, fetchNewProducts, fetchTopProducts, getProductById, updateProduct } from "./ProductController.js";
 
 
 export const product_routes = Router()
 
 
 product_routes.route("/product").post(authenticated, authorizedAdmin, formidable(), addProduct)
+product_routes.route("/products").get(allProductsWithSearchKeyword)
+product_routes.route("/allProducts").get(fetchAllProducts)
+
+
+
+product_routes.route("/:id/reviews").post(authenticated, checkId, addProductReview);
+
+
+product_routes.get("/top", fetchTopProducts);
+product_routes.get("/new", fetchNewProducts);
+
+
+product_routes.route("/product/:id")
+.get(getProductById)
+.put(authenticated, authorizedAdmin, formidable(), updateProduct)
+.delete(authenticated, authorizedAdmin, deleteProduct)
