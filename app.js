5870f4b8b702412c189e5e1120089cbc68ec0aa1
RahmaRubi <rahmaelroby47@gmail.com>
2025-02-11 00:18:29 +0200

    solved all conflicts ISA

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from 'express'
 import { dbConnection } from './db/dbConnection.js';
 import { user_routes } from './Modules/UserModule/UserRoutes.js';
+import { cart_routes } from './Modules/CartModule/CartRoutes.js';
 import cookieParser from 'cookie-parser';
 import category_routes from './Modules/CategoryModule/CategoryRoutes.js';
 import { product_routes } from './Modules/ProductModule/ProductRoutes.js';
-<<<<<<< Updated upstream
 import uploads_router from './Modules/uploadRoutes/uploadRoutes.js';
 import path from "path";
-=======
-import { cart_routes } from './Modules/CartModule/CartRoutes.js';
->>>>>>> Stashed changes
 
 dbConnection
 const app = express();
