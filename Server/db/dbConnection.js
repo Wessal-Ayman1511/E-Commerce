a80076c0b081aa6268eef2859681c645eff38581
Wessal Ayman <wessalayman17@gmail.com>
2025-02-14 16:55:47 +0200

    hosting db in mongo atlas

--- Server/db/dbConnection.js
+++ Server/db/dbConnection.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const dbConnection = mongoose.connect("mongodb://127.0.0.1:27017/E-Commerce-Backend").then(()=>{
+export const dbConnection = mongoose.connect("mongodb+srv://Wessal_Ayman:vxMHzgfbB7gNW525@cluster0.qv1hm.mongodb.net/E-Commerce-Backend").then(()=>{
     console.log("DB Connected Successfully");
 }).catch((err)=>{
     console.log(`Error Connecting to DB ${err}`);
